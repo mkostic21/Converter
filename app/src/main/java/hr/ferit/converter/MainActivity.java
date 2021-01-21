48234b00909a58cb0ebc149143578f07dd12cfbf
mkostic21 <mislav.kostic@gmail.com>
2021-01-21 15:59:22 +0100

    Basic fragment implementation

--- app/src/main/java/hr/ferit/converter/MainActivity.java
+++ app/src/main/java/hr/ferit/converter/MainActivity.java
@@ -1,14 +1,39 @@
 package hr.ferit.converter;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class MainActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private PagerAdapter pagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initViews();
+        setupPager();
+    }
+
+    private void initViews() {
+        tabLayout = findViewById(R.id.tab);
+        viewPager = findViewById(R.id.viewPager);
+
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    private void setupPager() {
+        pagerAdapter = new hr.ferit.converter.PagerAdapter(getSupportFragmentManager(),
+                FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        viewPager.setAdapter(pagerAdapter);
     }
 }
